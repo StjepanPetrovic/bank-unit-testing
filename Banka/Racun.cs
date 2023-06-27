df65af76f3b0a045c8775b496a48b0d8956b9d9b
Stjepan Petrović <stjepanpetrovic11@gmail.com>
2023-06-27 23:04:24 +0200

    Add project files.

---
+++ Banka/Racun.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banka
+{
+    public class Racun
+    {
+        public string IBAN { get; set; }
+        public double Stanje { get; set; }
+    }
+}
