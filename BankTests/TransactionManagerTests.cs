a6a85d0e228715a3d5b78be011b39d6652b03f73
Stjepan Petrović <stjepanpetrovic11@gmail.com>
2023-06-27 23:24:10 +0200

    Add Unit test project for testing

---
+++ BankTests/TransactionManagerTests.cs
@@ -0,0 +1,15 @@
+﻿using Banka;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace BankTests
+{
+    [TestClass]
+    public class TransactionManagerTests
+    {
+        [TestMethod]
+        public void TransactionManager()
+        {
+        }
+    }
+}
